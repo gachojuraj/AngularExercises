c7820a334d71c65555679a884b6e8267403489ba
gachojuraj <mmamma151@gmail.com>
2020-10-18 14:00:00 +0200

    finished heroes exercise

---
+++ src/app/mock-items.ts
@@ -0,0 +1,14 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+  { id: 1, name: 'Wooden Sword', price: 7 },
+  { id: 2, name: 'Fancy Hat', price: 130 },
+  { id: 3, name: 'Fast Boots', price: 90 },
+  { id: 4, name: 'Magic Book', price: 84 },
+  { id: 5, name: 'Heal Potion', price: 50 },
+  { id: 6, name: 'Bowling Ball', price: 23 },
+  { id: 7, name: 'Soda Can', price: 10 },
+  { id: 8, name: 'Paper Plane', price: 3 },
+  { id: 9, name: 'Pencil', price: 14 },
+  { id: 10, name: 'Water Gun', price: 74 }
+];
